000aa30c7bdecd09a8b5b7d7be0e156ac1081429
az <hereliesaz@gmail.com>
2025-10-20 00:02:48 -0500

    Initial commit

---
+++ app/src/main/kotlin/com/hereliesaz/magnom/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.hereliesaz.magnom
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.hereliesaz.magnom.navigation.Screen
+import com.hereliesaz.magnom.ui.screens.AdvancedRawDataEditorScreen
+import com.hereliesaz.magnom.ui.screens.CardEditorScreen
+import com.hereliesaz.magnom.ui.screens.MainScreen
+import com.hereliesaz.magnom.ui.screens.SettingsScreen
+import com.hereliesaz.magnom.ui.screens.TransmissionInterfaceScreen
+import com.hereliesaz.magnom.ui.theme.MagNomTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MagNomTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = Screen.Main.route) {
+                        composable(Screen.Main.route) { MainScreen(navController = navController) }
+                        composable(Screen.Editor.route) { CardEditorScreen(navController = navController) }
+                        composable(Screen.AdvancedEditor.route) { AdvancedRawDataEditorScreen(navController = navController) }
+                        composable(Screen.Settings.route) { SettingsScreen(navController = navController) }
+                        composable(Screen.Transmission.route) {
+                            TransmissionInterfaceScreen(navController = navController)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
